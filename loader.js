122882ef960bf41dc8a04a264569792c69523ac7
alex <alex@staticlibs.net>
2017-02-11 23:02:08 +0000

    require runner added

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@
 // https://github.com/requirejs/r.js/blob/27594a409b3d37427ec33bdc151ae8a9f67d6b2b/build/jslib/rhino.js
 
 (function () {
-    'use strict';
+    "use strict";
     require.load = function (context, moduleName, url) {
 
         load(url);
---
+++ runner.js
@@ -0,0 +1,25 @@
+
+function runScript(callbackScriptJson) {
+    try {
+        var cs = JSON.parse(callbackScriptJson);
+        if ("string" !== typeof(cs.module) || "string" !== typeof(cs.func) ||
+                "undefined" === typeof (cs.args) || !(cs.args instanceof Array)) {
+            throw new Error("Invalid 'callbackScriptJson' specified: [" + callbackScriptJson + "]");
+        }
+        var module;
+        // expected to be always sync
+        require([cs.module], function(mod) {
+            module = mod;
+        });
+        // target call
+        var res = module[cs.func].apply(module, cs.args);
+        if ("undefined" == typeof(res) || null === res) {
+            return "";
+        }
+        if ("string" !== typeof(res)) {
+            return JSON.stringify(res);
+        }
+    } catch(e) {
+        throw new Error(e.message + "\n" + "JS call data: [" + callbackScriptJson + "]" + "\n" + e.stack);
+    }
+}
