91bbae150dc0134f64c773117459ba7683dabd6b
alex <alex@staticlibs.net>
2017-10-27 13:19:35 +0100

    enable native buffers, disable native promises, fix jni utils

--- wilton-require.js
+++ wilton-require.js
@@ -113,60 +113,8 @@ setImmediate = function(fun, arg) { fun(arg);};
 // switch canEvaluate to false for bluebird
 navigator = null;
 
-// disable native arrays with
-// inconsistent cross-engine support
-ArrayBuffer = undefined;
-DataView = undefined;
-Int8Array = undefined;
-Uint8Array = undefined;
-Uint8ClampedArray = undefined;
-Int16Array = undefined;
-Uint16Array = undefined;
-Int32Array = undefined;
-Uint32Array = undefined;
-Float32Array = undefined;
-Float64Array = undefined;
+// use compat buffers
+Buffer = WILTON_requiresync("buffer").Buffer;
 
-// use compat buffers, may be not available in current modules set
-Buffer = (function() {
-    "use strict";
-    
-    try {
-        return WILTON_requiresync("buffer").Buffer;
-    } catch (e) {
-        return {};
-    }
-} ());
-
-// Nashorn Array.splice fix
-// https://bugs.openjdk.java.net/browse/JDK-8068972
-WILTON_Array_prototype_splice_orig = Array.prototype.splice;
-Array.prototype.splice = function() {
-    "use strict";
-    
-    if (1 === arguments.length) {
-        var args = Array.prototype.slice.call(arguments);
-        args.push(this.length - args[0]);
-        return WILTON_Array_prototype_splice_orig.apply(this, args);
-    }
-    return WILTON_Array_prototype_splice_orig.apply(this, arguments);
-};
-
-// Rhino Object.keys fix
-// "new String(...)" is not iterable with "for key in .." in Rhino
-WILTON_Object_keys_orig = Object.keys;
-Object.keys = function(obj) {
-    "use strict";
-    
-    if ("object" === typeof (obj) && obj instanceof String) {
-        var res = WILTON_Object_keys_orig(obj);
-        if (res.length !== obj.length) {
-            res = [];
-            for (var i = 0; i < obj.length; i++) {
-                res.push(String(i));
-            }
-        }
-        return res;
-    }
-    return WILTON_Object_keys_orig(obj);
-};
+// disable native promises is any
+Promise = undefined;
